2d62c98a30266d313dc30777398012cc6ca8b36b
Hugo Garbez <zaap59@gmail.com>
2016-06-05 16:27:27 +0200

    Add clothes.css

--- www/clothes/clothes.controller.js
+++ www/clothes/clothes.controller.js
@@ -10,7 +10,85 @@
 
   /* @ngInject */
   function ClothesController($scope) {
-
+    $scope.clothes = [
+        {
+            "id": 1,
+            "name": "Robe Tommy",
+            "categories": [
+                "Robe"
+            ],
+            "subcategories": [
+                "Longue Robe"
+            ],
+            "marque": "Tommy Hilfiger",
+            "colors": [
+                "bleu",
+                "rouge",
+                "blanc"
+            ],
+            "size": "38",
+            "picture": [
+                "https://s-media-cache-ak0.pinimg.com/236x/0d/49/53/0d49535360f0d3f64834d8a69db7343f.jpg"
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Robe Nafnaf",
+            "categories": [
+                "Robe"
+            ],
+            "subcategories": [
+                "Longue Robe"
+            ],
+            "marque": "Nafnaf",
+            "colors": [
+                "noire",
+                "blanc"
+            ],
+            "size": "38",
+            "picture": [
+                "https://photos6.spartoo.com/photos/104/1043058/1043058_1200_A.jpg"
+            ]
+        },
+        {
+            "id": 3,
+            "name": "Robe Tommy Hilfiger",
+            "categories": [
+                "Robe"
+            ],
+            "subcategories": [
+                "Longue Robe"
+            ],
+            "marque": "Tommy Hilfiger",
+            "colors": [
+                "bleu",
+                "blanc"
+            ],
+            "size": "38",
+            "picture": [
+                "http://photos6.shoes.fr/photos/817/817124/817124_350_A.jpg"
+            ]
+        },
+        {
+            "id": 4,
+            "name": "Robe Ralph Lauren",
+            "categories": [
+                "Robe"
+            ],
+            "subcategories": [
+                "Robe courte"
+            ],
+            "marque": "Ralph Lauren",
+            "colors": [
+                "bleu",
+                "blanc"
+            ],
+            "size": "38",
+            "picture": [
+                "http://www.venisedresscode.com/ekmps/shops/0814c8/images/polo-robe-ralph-lauren-[3]-1064-p.jpg"
+            ]
+        }
+    ]
 
   }
 
