9248cc50182b9ad63d673e995c99b1c788a67216
Theo59 <theoberkhout59@gmail.com>
2016-11-16 18:09:52 +0100

    feat(subcat): add subcat screen with picture

--- www/subcategory/subcategory.controller.js
+++ www/subcategory/subcategory.controller.js
@@ -9,8 +9,31 @@
     .controller('SubcategoryController', SubcategoryController);
 
   /* @ngInject */
-  function SubcategoryController($scope) {
+  function SubcategoryController($scope, $stateParams) {
 
+    $scope.clothes = [];
+
+    getClothes();
+
+    function getClothes () {
+      var Clothe = Parse.Object.extend("Clothe");
+      var query = new Parse.Query(Clothe);
+      query.equalTo("subCategorie", $stateParams.subcategoryName);
+      query.find({
+        success: function(results) {
+          angular.forEach(results, function(result) {
+            $scope.clothes.push({
+              id: result.id,
+              name: result.get('name'),
+              pictureUrl: result.get('picture')
+            });
+          });
+        },
+        error: function(error) {
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    }
 
   }
 
