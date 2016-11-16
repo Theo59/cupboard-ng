137aba41d3f9b99056f8b24c8d38319440f9683a
Ringeval <j.r62@hotmail.fr>
2016-11-16 15:22:54 +0100

    add Outfit

--- www/addOutfit/addOutfit.controller.js
+++ www/addOutfit/addOutfit.controller.js
@@ -9,8 +9,27 @@
     .controller('AddOutfitController', AddOutfitController);
 
   /* @ngInject */
-  function AddOutfitController($scope) {
+  function AddOutfitController($scope,$ionicModal) {
 
+    $ionicModal.fromTemplateUrl('addOutfit/modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+
+    $scope.addElement = function(){
+      document.getElementById('elementHead').innerHTML = "<i class='icon ion-happy'></i> test vetement";
+      $scope.modal.hide();
+    }
     // function saveOutfit(outfit) {
     //   OutfitService.save(outfit);
     // }
